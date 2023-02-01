bd25228b18ce394789392a0b5823f06245f09163
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2023-02-01 14:57:38 +0100

    Python client and server supports TLS handshake in which server authenticats client (as addition to standard validation of server by the client)

--- tls.py
+++ tls.py
@@ -3,24 +3,53 @@
 import socket, ssl, sys, os
 
 PORT= 10021
+CERT="domain.pem"
+KEY="domain.key"
 
 def client():
-    context = ssl.create_default_context()
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.load_verify_locations("domain.crt")
+    context.load_verify_locations(CERT)
+    conn = context.wrap_socket(socket.socket(socket.AF_INET),
+                               server_hostname="localhost")
+    conn.connect(("localhost", PORT))
+
+def client_can_authenticate_to_server():
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=CERT)
+    context.load_cert_chain(certfile=CERT, keyfile=KEY)
+    #context.load_verify_locations(CERT)
     conn = context.wrap_socket(socket.socket(socket.AF_INET),
                                server_hostname="localhost")
     conn.connect(("localhost", PORT))
     conn.send(b"ping!")
+    conn.send(b"ping!")
 
 def server():
-    #context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     context.load_default_certs()
-    context.load_cert_chain(certfile="domain.crt", keyfile="domain.key")
+    context.load_cert_chain(certfile=CERT, keyfile=KEY)
     bindsocket = socket.socket()
     bindsocket.bind(('localhost', PORT))
-    bindsocket.listen(5)
+    bindsocket.listen()
+
+    while True:
+        newsocket, fromaddr = bindsocket.accept()
+        connstream = context.wrap_socket(newsocket, server_side=True)
+        try:
+            deal_with_client(connstream)
+        finally:
+            connstream.shutdown(socket.SHUT_RDWR)
+            connstream.close()
+
+def server_auth_client():
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.verify_mode = ssl.CERT_REQUIRED
+
+    context.load_cert_chain(certfile=CERT, keyfile=KEY)
+    context.load_verify_locations(CERT)
+
+    bindsocket = socket.socket()
+    bindsocket.bind(('localhost', PORT))
+    bindsocket.listen()
 
     while True:
         newsocket, fromaddr = bindsocket.accept()
@@ -48,8 +77,8 @@ if len(sys.argv) < 2:
 
 match os.path.basename(sys.argv[1]):
     case "client":
-        client()
+        client_can_authenticate_to_server()
     case "server":
-        server()
+        server_auth_client()
 
 

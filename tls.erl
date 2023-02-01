f380785dd54bcff063fc6af82161d03cecfc6d96
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2023-02-01 16:08:45 +0100

    Erlang TLS client can authenticate server (but cannot authenticate itself to the server yet)

--- tls.erl
+++ tls.erl
@@ -4,8 +4,10 @@
 
 connect() ->
     ssl:start(),
-    {ok, Socket} = ssl:connect("localhost", 10021, [{verify, verify_none}, {cacertfile, "domain.pem"},  {active, once}], infinity),
-    ssl:send(Socket, "foo"),
+    {ok, Socket} = ssl:connect("localhost", 10022, [{verify, verify_none}, {cacertfile, "certs/server.pem"},  {active, once}], infinity),
+    %%timer:sleep(5 * 1000),
+    ok = ssl:send(Socket, "foo"),
+    %%timer:sleep(15 * 1000),
     ssl:close(Socket),
     ssl:stop().
 
